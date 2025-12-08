54be6087675caedf9d54af41bac0f36497ece0e9
Private Developer <tareknegoudi@gmail.com>
2025-12-08 16:55:28 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css"; // إذا كنت تستخدم Tailwind أو CSS عادي
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>

fa9ff9934d3d8773702516dddd887ccad49a85ea
Private Developer <tareknegoudi@gmail.com>
2025-12-12 15:51:05 +0100

    Create index.js

---
+++ backend/src/models/index.js
@@ -0,0 +1,21 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+const Message = require('./Message');
+const Notification = require('./Notification');
+const Story = require('./Story');
+const Group = require('./Group');
+const Event = require('./Event');
+const Report = require('./Report');
+
+module.exports = {
+  User,
+  Post,
+  Comment,
+  Message,
+  Notification,
+  Story,
+  Group,
+  Event,
+  Report
+};

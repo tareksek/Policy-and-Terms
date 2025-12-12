82729e36132227714247ed79302f598b68361e84
Private Developer <tareknegoudi@gmail.com>
2025-12-12 15:47:54 +0100

    Create Comment.js

---
+++ backend/src/models/Comment.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Post',
+    required: true
+  },
+  content: {
+    type: String,
+    required: true,
+    maxlength: 1000
+  },
+  parentComment: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Comment',
+    default: null
+  },
+  replies: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Comment'
+  }],
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  mentions: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  isEdited: {
+    type: Boolean,
+    default: false
+  },
+  editedAt: Date
+}, {
+  timestamps: true
+});
+
+commentSchema.index({ post: 1, createdAt: 1 });
+commentSchema.index({ user: 1 });
+
+module.exports = mongoose.model('Comment', commentSchema);
